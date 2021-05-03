fbee07115b687a67b9948a7a93c77b6e91fe6cb6
”Vladislav <monsterd161@gmail.com>
2021-05-03 21:44:30 +0300

    add 2.9

---
+++ JS/2.9/2.9.2/scripts/main.js
@@ -0,0 +1,86 @@
+let person = [
+  {
+    name: "Vladislav",
+    surname: "Berdnikov",
+    hw: "7",
+  },
+  {
+    name: "Alex",
+    surname: "Girko",
+    hw: "2",
+  },
+  {
+    name: "Artem",
+    surname: "Shevchuk",
+    hw: "8",
+  },
+  {
+    name: "Denis",
+    surname: "Rudik",
+    hw: "3",
+  },
+  {
+    name: "Alex",
+    surname: "Getmansk",
+    hw: "8",
+  },
+  {
+    name: "Inna",
+    surname: "Oleshenko",
+    hw: "7",
+  },
+  {
+    name: "Victor",
+    surname: "Victor",
+    hw: "3",
+  },
+  {
+    name: "Andrey",
+    surname: "Timoshenko",
+    hw: "6",
+  },
+  {
+    name: "Im",
+    surname: "Table",
+    hw: "6",
+  },
+  {
+    name: "Natalia",
+    surname: "Natalia",
+    hw: "4",
+  },
+  {
+    name: "Ivanna",
+    surname: "Ivanna",
+    hw: "0",
+  },
+  {
+    name: "Vova",
+    surname: "Vova",
+    hw: "6",
+  },
+  {
+    name: "R1d",
+    surname: "R1d",
+    hw: "4",
+  },
+  {
+    name: "Nikolay",
+    surname: "Zaichenko",
+    hw: "4",
+  },
+  {
+    name: "Andrew",
+    surname: "Stehnii",
+    hw: "1",
+  },
+  {
+    name: "Yaroslav",
+    surname: "Yaroslav",
+    hw: "4",
+  },
+];
+
+const person2 = person.map((val) => {
+  console.log(val);
+});
