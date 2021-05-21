4af4a47578e1bb7e8321a541391b524cce035be1
”Vladislav <monsterd161@gmail.com>
2021-05-21 19:07:49 +0300

    mod 3.1

--- Node.js/3.1/index.js
+++ Node.js/3.1/index.js
@@ -1,7 +1,15 @@
 const moment = require("moment");
-const momentRange = require("moment-precise-range-plugin");
 
 let birthDate = moment("07/29/2000", "MM/DD/YYYY");
-let birth = moment.preciseDiff(birthDate, moment());
+let birth = moment.duration(moment().diff(birthDate));
 
 console.log(birth);
+
+
+//const moment = require("moment");
+//const momentRange = require("moment-precise-range-plugin");
+
+//let birthDate = moment("07/29/2000", "MM/DD/YYYY");
+//let birth = moment.preciseDiff(birthDate, moment());
+
+//console.log(birth);
