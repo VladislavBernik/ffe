bca4d8eaec7fee9b874026906ef35cddc24c890c
”Vladislav <monsterd161@gmail.com>
2021-05-29 11:36:41 +0300

    add 3.4.1

---
+++ Node.js/3.4/3.4.1/index.js
@@ -0,0 +1,7 @@
+const axios = require("axios");
+
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?q=Odesa&units=metric&appid=999418815d68730b39a0858a86523528";
+axios.get(url).then((response) => {
+  console.log(response);
+});
