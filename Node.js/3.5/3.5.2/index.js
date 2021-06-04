8c923bc6d9bc19884b01bb5e4ef20400ba297b2c
”Vladislav <monsterd161@gmail.com>
2021-06-04 11:02:27 +0300

    add 3.5.2

---
+++ Node.js/3.5/3.5.2/index.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const fs = require("fs-extra");
+
+http
+  .createServer(function (request, response) {
+    console.log(`Запрошенный адрес: ${request.url}`);
+
+    const filePath = request.url.substr(1);
+    
+    fs.access(filePath, fs.constants.R_OK, (err) => {
+      if (err) {
+        response.statusCode = 404;
+        response.end("Resourse not found!");
+      } else {
+        fs.createReadStream(filePath).pipe(response);
+      }
+    });
+  })
+  .listen(3000);
