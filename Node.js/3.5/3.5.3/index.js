ca7187d343d35157d2e612a91d5d99ee1907d497
”Vladislav <monsterd161@gmail.com>
2021-06-05 12:59:09 +0300

    add 3.5.3

--- Node.js/3.5/3.5.3/index.js
+++ Node.js/3.5/3.5.3/index.js
@@ -3,6 +3,13 @@ const fs = require("fs-extra");
 
 http
   .createServer(function (req, res) {
+    const url = "http://vnokia.net/images/wallpaper/2019/wallpaper_1080x2160_03.jpg";
+
+    let file = fs.createWriteStream("file.jpeg");
+    let request = http.get(url, function (response) {
+      response.pipe(file);
+    });
+
     console.log(`Запрошенный адрес: ${req.url}`);
 
     const filePath = req.url.substr(1);
